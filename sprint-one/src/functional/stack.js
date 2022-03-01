d99a2e53191f1983fb0b483da2852290fa973103
Shiwei Chen <shiweichenus@gmail.com>
2022-03-01 00:01:06 -0600

    Complete functional stack and queue

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,27 @@
 var Stack = function() {
   var someInstance = {};
+  var index = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.pop = function() {
+    if (index > 0) {
+      index--;
+      var result = storage[index];
+      delete storage[index];
+      return result;
+    }
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
