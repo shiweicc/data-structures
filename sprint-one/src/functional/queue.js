05e6e1e543534df5a3676e16ff6898b5fdf4ff3a
Shiwei Chen <shiweichenus@gmail.com>
2022-02-28 23:14:03 -0600

    Complete functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,39 @@
 var Queue = function() {
   var someInstance = {};
-
+  var index = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.dequeue = function() {
+    // get the array of the numeric keys
+    // iterate over and find the small numeric key
+    // change each key from string to number
+    // delete the value of that property using the smallest numeric key
+    var keyArray = Object.keys(storage);
+
+    var smallestIndex = keyArray[0];
+    keyArray.forEach(function(item) {
+      var eachIndex = Number.parseInt(item);
+      if (eachIndex < smallestIndex) {
+        smallestIndex = eachIndex;
+      }
+    });
+
+    var result = storage[smallestIndex];
+    delete storage[smallestIndex];
+    return result;
+
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
